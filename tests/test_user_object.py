d3555f43fc3fc8900ece0abaa120e222522b34a7
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-03-01 23:12:57 +0100

    Test to test tests

--- tests/test_user_object.py
+++ tests/test_user_object.py
@@ -1,5 +1,16 @@
+import pytest
+
+from devlog.models import User
+from devlog.ext import db
+
 from . import DevlogTests
 
 
+@pytest.mark.usefixtures('client_class')
 class TestUserObject(DevlogTests):
-    pass
+
+    def test_defaults(self):
+        user = User(email='test.email@example.com')
+        db.session.add(user)
+        db.session.commit()
+        assert user.id is not None
