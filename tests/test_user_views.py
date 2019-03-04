52c76be1e4bb23c53c1209bea0eb41889bfdbe07
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-03-04 13:04:09 +0100

    Couple more tests

--- tests/test_user_views.py
+++ tests/test_user_views.py
@@ -12,13 +12,27 @@ class TestUserViews(DevlogTests):
     def set_up(self):
         self.profile_url = url_for('user.profile')
 
-    def test_user_profile_anon(self):
+    def test_user_profile_anon_view(self):
         rv = self.client.get(self.profile_url)
         assert rv.status_code == 302
 
-    def test_user_profile_authenticated(self, user_factory):
+    def test_user_profile_authenticated_view(self, user_factory):
         user = user_factory(name='Ivory Tower')
         self.login(email=user.email)
         rv = self.client.get(self.profile_url)
         page = rv.data.decode('utf-8')
         assert 'value="{}"'.format(user.name) in page
+
+    def test_user_profile_anon_update(self):
+        data = {'name': 'New Name'}
+        rv = self.client.post(self.profile_url, data=data)
+        assert rv.status_code == 302
+
+    def test_user_profile_authenticated_update(self, user_factory):
+        user = user_factory(name='Ivory Tower')
+        self.login(email=user.email)
+        new_name = 'Infernal Amendment'
+        data = {'name': new_name}
+        rv = self.client.post(self.profile_url, data=data, follow_redirects=True)
+        page = rv.data.decode('utf-8')
+        assert 'value="{}"'.format(new_name) in page
