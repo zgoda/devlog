0bb8a6f5d56f5acc1315fa56f77133cbf0b1787f
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-03-15 23:21:15 +0100

    Self rendering button element

--- src/devlog/utils/forms.py
+++ src/devlog/utils/forms.py
@@ -1,4 +1,5 @@
 import attr
+from flask import render_template_string
 from flask_babel import lazy_gettext as gettext
 from flask_wtf import FlaskForm
 from wtforms.fields import BooleanField
@@ -6,14 +7,27 @@ from wtforms.fields import BooleanField
 from ..ext import db
 
 
+class Clickable:
+
+    def render(self):
+        return render_template_string(self.template, obj=self)
+
+
 @attr.s
-class Button:
+class Button(Clickable):
     type_ = attr.ib(default='submit')
     class_ = attr.ib(default='primary')
     icon = attr.ib(default='check')
     icon_type = attr.ib(default='fas')
     text = attr.ib('ok')
     link = attr.ib(default=False)
+
+    template = '''
+<button type="{{ obj.type_ }}" class="btn btn-{{ obj.class_ }}">
+    <i class="{{ obj.icon_type }} fa-{{ obj.icon }}"></i>
+    &nbsp;
+    {{ obj.text }}
+</button>'''.strip()
 
 
 class DeleteForm(FlaskForm):
