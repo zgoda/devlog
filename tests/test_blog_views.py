fcc7c60bb50d345103f92bbbef54e9bf24246cf7
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-03-16 21:31:09 +0100

    Tests for blog details anon access

--- tests/test_blog_views.py
+++ tests/test_blog_views.py
@@ -34,7 +34,7 @@ class TestBlogCreateView(DevlogTests):
         assert rv.status_code == 302
         assert url_for('auth.select') in rv.headers['Location']
 
-    def test_authenticated_post(self, user_factory):
+    def test_authenticated_post_ok(self, user_factory):
         user = user_factory(name='Ivory Tower')
         blog_name = 'Infernal Tendencies'
         data = {
@@ -45,3 +45,36 @@ class TestBlogCreateView(DevlogTests):
         rv = self.client.post(self.url, data=data, follow_redirects=True)
         assert f'{blog_name} has been created' in rv.text
         assert f'<h1>{blog_name}</h1>' in rv.text
+
+    def test_authenticated_post_fail(self, user_factory):
+        user = user_factory(name='Ivory Tower')
+        data = {
+            'user': user.id,
+        }
+        self.login(user.email)
+        rv = self.client.post(self.url, data=data, follow_redirects=True)
+        assert f'field is required' in rv.text
+        assert f'has been created' not in rv.text
+
+
+@pytest.mark.usefixtures('client_class')
+class TestBlogDetailsView(DevlogTests):
+
+    def url(self, blog):
+        return url_for('blog.display', blog_id=blog.id)
+
+    def test_anon_get_public(self, blog_factory):
+        blog = blog_factory(name='Infernal Tendencies')
+        rv = self.client.get(self.url(blog))
+        assert rv.status_code == 200
+        assert f'<h1>{blog.name}</h1>' in rv.text
+
+    def test_anon_get_private(self, blog_factory):
+        blog = blog_factory(name='Infernal Tendencies', public=False)
+        rv = self.client.get(self.url(blog))
+        assert rv.status_code == 404
+
+    def test_anon_get_inactive(self, blog_factory):
+        blog = blog_factory(name='Infernal Tendencies', active=False)
+        rv = self.client.get(self.url(blog))
+        assert rv.status_code == 404
