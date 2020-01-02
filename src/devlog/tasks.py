1b4fadfe22039d97995d3ae7d819ec7e96c31cd3
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-01-02 17:03:36 +0100

    Rewrite task code to not require application context

--- src/devlog/tasks.py
+++ src/devlog/tasks.py
@@ -1,33 +1,44 @@
+import os
 import re
+from datetime import datetime
 
 import markdown
 import pytz
+import sqlalchemy as sa
 from dateutil.parser import isoparse
 
-from .app import make_app
-from .models import Blog, Post, db
-
-app = make_app()
-app.app_context().push()
+from .utils.text import slugify, stripping_markdown
 
 
 METADATA_RE = re.compile(r'\A---(.|\n)*?---')
 
 
 def import_post(file_name: str, blog_id: int):
+    engine = sa.create_engine(os.environ['SQLALCHEMY_DATABASE_URI'])
+    cn = engine.connect()
     try:
         with open(file_name) as fp:
             text = fp.read()
-        md = markdown.Markdown(extensions=['meta'])
-        md.convert(text)
+        md = markdown.Markdown(
+            extensions=['meta', 'fenced_code', 'codehilite'], output_format='html'
+        )
+        html_text = md.convert(text)
         if not md.Meta or not md.Meta.get('title'):
             raise ValueError(
                 f'Post file {file_name} does not provide post title in metadata'
             )
-        blog = Blog.query.get(blog_id)
-        if blog is None:
+        blog_sql = sa.text(
+            'select id, user_id from blog where id = :blog_id'
+        ).bindparams(blog_id=blog_id)
+        blog_rv = cn.execute(blog_sql)
+        row = blog_rv.first()
+        if row is None:
             raise ValueError(f'Blog ID {blog_id} not found')
-        plain_text = METADATA_RE.sub('', text, count=1).strip()
+        user_id = row[1]
+        plain_content = METADATA_RE.sub('', text, count=1).strip()
+        sm = stripping_markdown()
+        plain_text = sm.convert(plain_content)
+        summary = ' '.join(plain_text.split()[:10])
         title = ' '.join(md.Meta['title']).strip()
         title = title.replace("'", '')
         created_dt = None
@@ -35,18 +46,32 @@ def import_post(file_name: str, blog_id: int):
         if post_date:
             created_dt = isoparse(post_date)
             if created_dt.tzinfo is None:
-                tz = pytz.timezone(app.config['BABEL_DEFAULT_TIMEZONE'])
+                tz = pytz.timezone(
+                    os.environ.get('BABEL_DEFAULT_TIMEZONE', 'Europe/Warsaw')
+                )
                 created_dt = created_dt.astimezone(tz).astimezone(pytz.utc)
             else:
                 created_dt = created_dt.astimezone(pytz.utc)
         is_draft = ' '.join(md.Meta.get('draft', [])).strip()
         is_draft = 'false' not in is_draft.lower()
-        post = Post(
-            blog=blog, title=title, text=plain_text, created=created_dt, draft=is_draft
+        updated = datetime.utcnow()
+        published = None
+        if not is_draft:
+            published = updated
+        post_sql = sa.text(
+            '''
+            insert into post (
+                blog_id, author_id, title, slug, text, text_html,
+                created, updated, published, draft, summary
+            ) values (
+                :blog_id, :author_id, :title, :slug, :text, :text_html,
+                :created, :updated, :published, :draft, :summary
+            )'''
+        ).bindparams(
+            blog_id=blog_id, author_id=user_id, title=title, slug=slugify(title),
+            text=plain_content, text_html=html_text, created=created_dt,
+            updated=updated, published=published, draft=is_draft, summary=summary
         )
-        db.session.add(post)
-        db.session.commit()
-    except Exception:
-        app.logger.exception(
-            f'Unhandled exception when importing post file {file_name}'
-        )
+        cn.execute(post_sql)
+    finally:
+        cn.close()
