c479c42ae6c8bcb241a5a92f7c4ab0a12d0a185f
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-06-24 00:11:55 +0200

    Home page template made nicer

--- src/devlog/views.py
+++ src/devlog/views.py
@@ -1,8 +1,16 @@
 from flask import render_template, Blueprint
 
+from .models import Post
+
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    posts = (
+        Post.select()
+        .where(Post.published.is_null(False))
+        .order_by(Post.published.desc())
+        .limit(5)
+    )
+    return render_template('index.html', posts=posts)
