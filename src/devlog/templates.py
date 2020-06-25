6ffd5379010c05052ae1b1ca45ca539645bff2f5
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-06-25 23:36:09 +0200

    We don't have wtforms for now

--- src/devlog/templates.py
+++ src/devlog/templates.py
@@ -1,5 +1,4 @@
 from flask_babel import format_datetime
-from wtforms.fields import HiddenField
 
 from ._version import get_version
 from .utils.app import Devlog
@@ -15,7 +14,6 @@ def setup_globals(application: Devlog):
         'format_datetime': format_datetime,
         'url_for_other_page': url_for_other_page,
         'version': get_version(),
-        'is_hidden_field': lambda x: isinstance(x, HiddenField),
     })
 
 
