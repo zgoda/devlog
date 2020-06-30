cc545d20407d6dc44feb202fdae3d2a13495362e
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-06-30 23:48:33 +0200

    Integrate Flask-Assets (webassets)

--- src/devlog/ext.py
+++ src/devlog/ext.py
@@ -1,5 +1,13 @@
+from flask_assets import Bundle, Environment
 from flask_babel import Babel
 from flask_flatpages import FlatPages
 
 babel = Babel()
 pages = FlatPages()
+assets = Environment()
+
+_css = Bundle(
+    'css/app.css', 'css/pgm_friendly.css', 'vendor/normalize.css',
+    filters='cssmin', output='gen/packed.css',
+)
+assets.register('app_css', _css)
