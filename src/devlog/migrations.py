6b6e3f4c50c91f57d0082c301f1c38aaafe1aca0
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-07-15 11:30:33 +0200

    Add description field to post table
    Also 1st attempt to do schema migrations with Peewee.

---
+++ src/devlog/migrations.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from peewee import TextField
+from playhouse.migrate import SqliteMigrator, migrate
+
+from .models import db
+
+
+def add_post_description():
+    field = TextField(null=True)
+    migrator = SqliteMigrator(db)
+    migrate(
+        migrator.add_column('post', 'description', field)
+    )
+
+
+_MIGRATIONS = {
+    'add_post_description': add_post_description
+}
+
+
+def run_migration(name):
+    migration = _MIGRATIONS[name]
+    migration()
