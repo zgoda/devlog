295249954c5be7138ac176168d5aa367d88baba6
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-01-08 23:18:19 +0100

    Quip collection view

--- src/devlog/api/views.py
+++ src/devlog/api/views.py
@@ -1,9 +1,12 @@
-from flask import request
+import markdown
+from flask import g, request
 
 from ..models import Quip, User
+from ..utils.pagination import get_page
+from ..utils.text import PostProcessor
 from . import api_bp as bp
-from .utils import generate_token, json_error_response, token_required
 from .schema import quip_schema
+from .utils import generate_token, json_error_response, token_required
 
 
 @bp.route('/login', methods=['POST'])
@@ -16,8 +19,16 @@ def login():
     return json_error_response(404, 'No such account')
 
 
-@bp.route('/quips', endpoint='quip-collection-get')
+@bp.route('/quips', endpoint='quip-collection', methods=['GET', 'POST'])
 @token_required
-def get_list():
-    items = Quip.select().order_by(Quip.created.desc())
-    return {'quips': quip_schema.dump(items, many=True)}
+def quip_collection():
+    if request.method == 'GET':
+        page = get_page()
+        items = Quip.select().paginate(page, 100).order_by(Quip.created.desc())
+        return {'quips': quip_schema.dump(items, many=True)}
+    data = quip_schema.load(request.get_json())
+    quip = Quip(**data)
+    quip.author = g.user.name
+    quip.text_html = markdown.markdown(quip.text, **PostProcessor.MD_KWARGS)
+    quip.save()
+    return {'quip': quip_schema.dump(quip)}, 201
